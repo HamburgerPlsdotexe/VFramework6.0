0cf8d15cddae4e8ffae060e11842eb3212ff848a
Çaglar <caglar_G@live.nl>
2022-06-07 15:57:24 +0200

    Newest update w nameGen bound

---
+++ Utility/Debug.cs
@@ -0,0 +1,30 @@
+﻿
+namespace VFBlazor6._0.Utility
+{
+    internal static class Debug
+    {
+        internal static void PrintDir()
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+
+        internal static void PrintDirTree()
+        {
+            foreach (var items in Directory.GetFiles(Directory.GetCurrentDirectory()))
+            {
+                Console.WriteLine(items);
+            }
+        }
+        internal static void PrintDictContents(Dictionary<string, string> dictionary)
+        {
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                if(dictionary.Count == 0)
+                {
+                    Console.WriteLine("\nDict is empty\n");
+                }
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
---
+++ Utility/FileOps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace VFBlazor6._0.Utility
+{
+    static class FileOps
+    {
+        internal static string _solutionDir = Directory.GetCurrentDirectory();
+
+        internal static void CleanUpOutFolder()
+        {
+            try
+            {
+                string path = _solutionDir + @"\cdktf.out\stacks\azure\";
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        internal static Dictionary<string, string> JsonFileToDict (string fileLoc)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(fileLoc));
+            }
+
+            catch(Exception e)
+            {
+                return new Dictionary<string, string>() { { "error" , e.ToString() } };
+            }
+        }
+    }
+}
